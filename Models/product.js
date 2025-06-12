e705c657705d59d4a6cd70ec17560d189c9858d1
Jagruti Dhole <jagrutidhole18@gmail.com>
2025-06-12 15:06:45 +0530

    Add .gitignore file

--- Models/product.js
+++ Models/product.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  description: String
-});
+  title: { type: String, required: true },
+  description: String,
+  price: { type: Number, required: true },
+  category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
+  imageURL: String,
+  arModel: { type: mongoose.Schema.Types.ObjectId, ref: 'ARModel' }, 
+  voiceCommands: [{ type: mongoose.Schema.Types.ObjectId, ref: 'VoiceCommand' }], 
+  stock: { type: Number, default: 0 },
+  tags: [String]
+}, { timestamps: true });
 
 const Product = mongoose.model('Product', productSchema);
 export default Product;
