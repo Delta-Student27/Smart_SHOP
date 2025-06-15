5fb86cb4dcf43dadf54dee54c1edc30926200be4
Delta-Student27 <tanushripatil26@gmail.com>
2025-06-15 19:10:48 +0530

    Added middleware

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import connectDB from './config/db.js';  // Import the DB connection function
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+import cors from 'cors';
+import connectDB from './config/db.js';
+
+ 
 import Product from './Models/product.js';
 import User from './Models/User.js';
 import Reward from './Models/Reward.js';
@@ -11,16 +13,34 @@ import Order from './Models/Order.js';
 import Feedback from './Models/Feedback.js';
 import AssistantHistory from './Models/AssistantHistory.js';
 import ARModel from './Models/ARModel.js';
-import VoiceCommand from './Models/VoiceCommand.js'
+import VoiceCommand from './Models/VoiceCommand.js';
 
+ 
 dotenv.config();
 
+ 
 const app = express();
-const PORT = 7890;
+const PORT = process.env.PORT || 7890;
 
-// Connect to MongoDB before starting the server
+// Connect to MongoDB
 connectDB();
 
+// Middleware
+app.use(cors());  
+app.use(express.json());  
+
+ 
+app.get('/', (req, res) => {
+  res.send('Welcome to SmartShop API');
+});
+
+ 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: err.message || 'Internal Server Error' });
+});
+
+// Start Server
 app.listen(PORT, () => {
-  console.log(`Server is Listening at http://localhost:${PORT}...`);
+  console.log(`Server is listening at http://localhost:${PORT}`);
 });
